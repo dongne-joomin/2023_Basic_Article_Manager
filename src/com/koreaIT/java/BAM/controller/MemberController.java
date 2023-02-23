b1fe9a263b200cda7cf89aced08d9c97c819b151
dongne-joomin <donghunc88@gmial.com>
2023-02-23 13:49:16 +0900

    로그인 기능 구현(문제점 다수 존재)

--- src/com/koreaIT/java/BAM/controller/MemberController.java
+++ src/com/koreaIT/java/BAM/controller/MemberController.java
@@ -28,6 +28,10 @@ public class MemberController extends Controller{
 		case "join":
 			doJoin();
 			break;
+		case "login":
+			dologin();
+			break;
+			
 		default:
 			System.out.println("존재하지 않는 명령어 입니다.");
 			break;
@@ -66,6 +70,7 @@ public class MemberController extends Controller{
 			}
 			break;
 		}
+		
 
 		System.out.printf("이름 : ");
 		String name = sc.nextLine();
@@ -76,15 +81,42 @@ public class MemberController extends Controller{
 		System.out.printf("%s 회원님 환영합니다.\n", loginId);
 	}
 
-	private boolean loginIdDupChk(String loginId) {
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return false;
-			}
+	private void dologin() {
+		
+		System.out.printf("로그인 아이디 : ");
+		String loginId = sc.nextLine();
+		System.out.printf("로그인 비밀번호 : ");
+		String loginPw = sc.nextLine();
+		
+		Member member = getMemberByLoginId(loginId);
+		
+		if (member == null) {
+			System.out.println("존재하지 않는 아이디 입니다.");
+			return;
 		}
-		return true;
+		if(member.loginPw.equals(loginPw) == false) {
+			System.out.println("비밀번호를 확인해주세요.");
+			return;
+		}
+		System.out.printf("%s님 환영합니다.\n", member.name);
 	}
 
+	private Member getMemberByLoginId(String loginId) {
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return member;
+			}
+		}
+		return null;
+	}
 
+	private boolean loginIdDupChk(String loginId) {
+		Member member = getMemberByLoginId(loginId);
+		
+		if(member == null) {
+			return true;
+		}
+		return false;
+	}
 
 }
